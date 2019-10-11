9f85f0bad2eb95862854ce2c2130b74f4d965e9f
G. Laudanno <P274829@workspace.rug.nl>
2019-10-11 15:25:21 +0200

    We got interesting data!

--- scripts/run_all.R
+++ scripts/run_all.R
@@ -1,5 +1,5 @@
-crown_age <- 8.1
+crown_age <- 8
 saveit <- TRUE
-measure_taxa_and_mbness(n_replicates = 30, crown_age = crown_age, saveit = saveit)
+measure_taxa_and_mbness(n_replicates = 1e4, crown_age = crown_age, saveit = saveit)
 plot_taxa_and_mbness(crown_age = crown_age, saveit = saveit)
 check_taxa_and_mbness(crown_age = crown_age)
